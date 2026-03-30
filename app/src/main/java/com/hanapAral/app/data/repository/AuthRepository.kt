80dc799a099966d40814abbf6af5bf29f1e7efca
Pamintuan <russellerojo34@gmail.com>
2026-03-30 13:10:22 +0800

    ◦Commit message: feat: implement google sign-in credential logic

---
+++ app/src/main/java/com/hanapAral/app/data/repository/AuthRepository.kt
@@ -0,0 +1,12 @@
+package com.hanapAral.app.data.repository
+
+import android.content.Context
+import android.content.Intent
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.hanapAral.app.R
+import kotlinx.coroutines.tasks.await
