1bc811a6e21833af88df29efcf2e8789fa8c5409
Dxtrply-crypto <dexter.pelayo.john@gmail.com>
2026-03-31 22:29:17 +0800

    Pelayo- Commiting the ui for CreateGroupScreen.kt codes.

---
+++ app/src/main/java/com/hanapAral/app/ui/screens/CreateGroupScreen.kt
@@ -0,0 +1,18 @@
+package com.hanapAral.app.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.hanapAral.app.R
