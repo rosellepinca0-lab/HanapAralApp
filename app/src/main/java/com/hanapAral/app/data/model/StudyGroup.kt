8b077fa5cc886fc20397d29f8147ebada6bbc69e
Paolo <pprincepaolo@gmail.com>
2026-03-31 23:14:36 +0800

    define groups collection structure

---
+++ app/src/main/java/com/hanapAral/app/data/model/StudyGroup.kt
@@ -0,0 +1,9 @@
+package com.hanapAral.app.data.model
+
+data class StudyGroup(
+    val id: String = "",
+    val name: String = "",
+    val subject: String = "",
+    val description: String = "",
+
+)
