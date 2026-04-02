80203a7f4adc81f8e6ccf8a40a1fcd289b060011
Paolo <pprincepaolo@gmail.com>
2026-04-02 14:56:32 +0800

    feat: logic to save and update fcm tokens in firestore

--- app/src/main/java/com/hanapAral/app/data/repository/UserRepository.kt
+++ app/src/main/java/com/hanapAral/app/data/repository/UserRepository.kt
@@ -12,3 +12,18 @@ class UserRepository {
     suspend fun saveUser(user: User) {
         usersCollection.document(user.uid).set(user).await()
     }
+
+    suspend fun getUser(uid: String): User? {
+        val doc = usersCollection.document(uid).get().await()
+        return doc.toObject(User::class.java)
+    }
+
+    suspend fun profileExists(uid: String): Boolean {
+        val doc = usersCollection.document(uid).get().await()
+        return doc.exists()
+    }
+
+    suspend fun updateFcmToken(uid: String, token: String) {
+        usersCollection.document(uid).update("fcmToken", token).await()
+    }
+}
